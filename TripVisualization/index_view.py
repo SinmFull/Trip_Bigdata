796234f6380ec5d0d1bacee296e2ebad8970b61d
full_sinm <1747373447@qq.com>
2023-07-10 09:53:21 +0800

    初始化

---
+++ TripVisualization/index_view.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.views.decorators import csrf
+import pymongo
+from . import setting
+import random
+from django.http import JsonResponse
+# from django.conf import settings
+
+# settings.configure()
+
+def index(request):  # index页面加载
+    context = {}
+    return render(request, 'index.html', context)
+
+def get_poi_data(request):
+    # 建立与MongoDB的连接
+    client = pymongo.MongoClient(host=setting.MONGODB_HOST, port=setting.MONGODB_PORT)
+    db = client[setting.MONGODB_NAME]
+    collection = db['my_col']
+    print(f'连接MongoDB {setting.MONGODB_NAME}.{collection.name} 成功')
+
+    poi_data = list(collection.find({}, {'_id': 0}))
+    # print(poi_data[0:10])
+    new_list = random.sample(poi_data,5000)
+    sorted_list = sorted(new_list, key=lambda x: x['properties']['hot_score'],reverse=True)
+    # print(sorted_list[0])
+    geojson = {'type': 'FeatureCollection', 'features': sorted_list}
+    return JsonResponse(geojson, safe=False)
+
+def post_tag_data(request):
+    if request.method == 'POST':
+        # 获取前端发送的数据
+        key1 = request.POST.get('key1')
+        
+        # 在此处进行数据处理逻辑
+        # ...
+        
+        # 返回响应给前端
+        response_data = {
+            'message': 'Data received and processed successfully!'
+        }
+        return JsonResponse(response_data)
+    return {'a':'1'}
